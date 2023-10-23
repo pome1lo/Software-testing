73b83658178db43e894bb11ea68e007f0ba17d70
Alexey <95231870+pome1lo@users.noreply.github.com>
2023-10-23 22:58:31 +0300

    add lw7 #2

---
+++ lw7/Calc.Tests/Other.cs
@@ -0,0 +1,29 @@
+﻿namespace Calc.Tests
+{
+    public class Other
+    {
+        private Calc calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.calc = new();
+        }
+
+        [Test]
+        public void GetLastResultAfterOperation()
+        {
+            var result = calc.Calculate(3.33, 1.11, "/");
+            var expectedResult = Is.EqualTo(calc.GetLastReuslt());
+            Assert.That(result, expectedResult); 
+        }
+
+        [Test]
+        public void GetLastResultBeforeOperation()
+        {
+            var result = 0;
+            var expectedResult = Is.EqualTo(calc.GetLastReuslt());
+            Assert.That(result, expectedResult);
+        }
+    }
+}
---
+++ lw7/Calc.Tests/TypicalErrors.cs
@@ -0,0 +1,53 @@
+﻿namespace Calc.Tests
+{ 
+    public class TypicalErrors
+    {
+        private Calc calc;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.calc = new();
+        }
+
+        [Test]
+        public void DevidedByZero()
+        {
+            Assert.Throws<Exception>(
+                () => new Calc().Calculate(10, 0, "/")
+            );
+        }
+
+        [Test]
+        public void NegativeMultiplication()
+        {
+            var result = calc.Calculate(-5, -5, "*");
+            var expectedResult = Is.EqualTo(25);
+            Assert.That(result, expectedResult); 
+        }
+
+        [Test]
+        public void WithoutParameters_String()
+        {
+            Assert.Throws<Exception>(
+              () => new Calc().Calculate(0, 0, "error")
+            );
+        }
+
+        [Test]
+        public void WithoutParameters_Char()
+        {
+            Assert.Throws<Exception>(
+                () => new Calc().Calculate(0, 0, "&")
+            );
+        }
+
+        [Test]
+        public void WithoutParameters_Char_2arguments()
+        {
+            Assert.Throws<Exception>(
+              () => new Calc().Calculate(0, "&")
+            );
+        }
+    }
+}
